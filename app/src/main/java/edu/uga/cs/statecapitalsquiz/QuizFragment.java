cca914b3125b024baea42df7a9323f69da3ed5e8
George Tindle <gtindle@Georges-MacBook-Pro.local>
2024-11-04 20:59:20 -0500

    finished fragment additions & implemented review past quizzes XML

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/QuizFragment.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/QuizFragment.java
@@ -21,11 +21,32 @@ public class QuizFragment extends Fragment {
     private ViewPager2 viewPager;
     private QuizPagerAdapter quizPagerAdapter;
 
+    /**
+     * Inflates the ViewPager2 container where quiz question fragments are held.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to. The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
         return inflater.inflate( R.layout.quiz_pager, container, false );
     }
 
+    /**
+     * After inflating the container, create a new quiz pager adapter which will manage the
+     * ViewPager2 and assist in swiping capabilities.
+     *
+     * @param view The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     */
     @Override
     public void onViewCreated( View view, Bundle savedInstanceState ) {
         super.onViewCreated( view, savedInstanceState );
--- app/src/main/java/edu/uga/cs/statecapitalsquiz/ReviewFragment.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/ReviewFragment.java
@@ -7,10 +7,27 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+/**
+ * This fragment class is designed to represent a list of past quiz results, specifically including
+ * a past quiz's date, time, and score.
+ */
 public class ReviewFragment extends Fragment {
+    /**
+     * Inflates the review_layout.
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to. The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return The inflated splash_layout.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+// Inflate the layout for this fragment
         return inflater.inflate(R.layout.review_layout, container, false);
     }
 }
